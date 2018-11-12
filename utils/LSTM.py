73b2f3414771c3b89cb68d21fe1468c0dc8607ab
nwesem <niclas.wesemann@tum.de>
2018-11-12 23:08:19 +0100

    working in many2many lstm

--- utils/LSTM.py
+++ utils/LSTM.py
@@ -4,25 +4,26 @@ from torch.nn import functional as F
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# Many-2-One LSTM
 class LSTM(nn.Module):
     def __init__(self, batch_size=7, lstm_layers=2, hidden_size=32, seq_length=7, input_size=100):
         super(LSTM, self).__init__()
         
+        # INIT
         self.batch_size = batch_size
         self.hidden_size = hidden_size
         self.lstm_layers = lstm_layers
         self.seq_length = seq_length
         self.input_size = input_size
         
-        ###LSTM###########
+        # LSTM
         self.lstm = nn.LSTM(input_size=self.hidden_size, hidden_size=self.hidden_size,
                             num_layers=self.lstm_layers, batch_first=True, dropout=0.3)
-        ##################
         
-        ###LINEAR LAYERS###
+        #LINEAR LAYERS 
         self.i2h = nn.Linear(self.input_size, self.hidden_size)
         self.h2o = nn.Linear(self.hidden_size,self.input_size)
-        ###################    
+   
     
     def reorder_batch(self, embedded_seqs):
         return torch.stack(torch.chunk(embedded_seqs, int(self.batch_size/self.seq_length)))
@@ -33,8 +34,6 @@ class LSTM(nn.Module):
                            self.hidden_size, dtype=torch.double).to(device)
 
     def forward(self, embed, future = 0):
-        print(self)
-        print(self.batch_size, self.seq_length, self.hidden_size)
         h_t0 = self.hidden_init()
         c_t0 = self.hidden_init()
         
@@ -43,5 +42,43 @@ class LSTM(nn.Module):
         output = self.h2o(output[:,:,:])
         
         return embed, output[:,-1,:]
+
+# Many-2-Many LSTM
+
+class LSTM_Many2Many(nn.Module):
+    def __init__(self, batch_size=7, lstm_layers=2, hidden_size=32, seq_length=7, input_size=100):
+        super(LSTM_Many2Many, self).__init__()
+        
+        # INIT
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+        self.lstm_layers = lstm_layers
+        self.seq_length = seq_length
+        self.input_size = input_size
+        
+        # LSTM
+        self.lstm = nn.LSTM(input_size=self.hidden_size, hidden_size=self.hidden_size,
+                            num_layers=self.lstm_layers, batch_first=True, dropout=0.3)
+        
+        # Linear layers
+        self.i2h = nn.Linear(self.input_size, self.hidden_size)
+        self.h2o = nn.Linear(self.hidden_size,self.input_size)    
+    
+    def reorder_batch(self, embedded_seqs):
+        return torch.stack(torch.chunk(embedded_seqs, int(self.batch_size/self.seq_length)))
+         
+    def hidden_init(self):
+        return torch.zeros(self.lstm_layers, int(self.batch_size/self.seq_length), 
+                           self.hidden_size, dtype=torch.double).to(device)
+
+    def forward(self, embed, future = 0):#, lenghts):
+        h_t0 = self.hidden_init()
+        c_t0 = self.hidden_init()
+        
+        lstm_input = self.i2h(embed)
+        output, (h_t1, c_t1) = self.lstm(lstm_input, (h_t0, c_t0))
+        output = self.h2o(output[:,:,:])
+        
+        return embed, output
 
     
